d01a5f4332c9d9f516dea6286ce8ed7b9257a470
DeyvidJLira <deyvidjlira@gmail.com>
2016-10-26 22:03:45 -0200

    Create constants

---
+++ app/src/main/java/br/com/deyvidjlira/popularmovies/util/Constants.java
@@ -0,0 +1,14 @@
+package br.com.deyvidjlira.popularmovies.util;
+
+/**
+ * Created by Deyvid on 25/10/2016.
+ */
+public final class Constants {
+    public final static String BASE_URL = "https://api.themoviedb.org/";
+    public final static String APP_KEY_PARAM = "api_key";
+    public final static String SORT_LATEST_PARAM = "latest";
+    public final static String SORT_POPULAR_PARAM = "popular";
+    public final static String SORT_RATE_PARAM = "top_rated";
+    public final static String IMAGE_BASE_URL= "https://image.tmdb.org/t/p/";
+    public final static String IMAGE_SMALL_SIZE = "w185";
+}
