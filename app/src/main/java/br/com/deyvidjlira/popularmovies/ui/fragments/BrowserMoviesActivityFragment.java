b264d246139196c04ff53d54df7c75415ae55243
DeyvidJLira <deyvidjlira@gmail.com>
2016-10-29 16:45:51 -0200

    Create FetchMoviesTask

--- app/src/main/java/br/com/deyvidjlira/popularmovies/ui/fragments/BrowserMoviesActivityFragment.java
+++ app/src/main/java/br/com/deyvidjlira/popularmovies/ui/fragments/BrowserMoviesActivityFragment.java
@@ -5,6 +5,7 @@ import android.content.Context;
 import android.content.Intent;
 import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
@@ -29,7 +30,6 @@ import br.com.deyvidjlira.popularmovies.ui.activities.DetailMovieActivity;
 import br.com.deyvidjlira.popularmovies.ui.adapters.MovieAdapter;
 import br.com.deyvidjlira.popularmovies.util.Constants;
 import retrofit2.Call;
-import retrofit2.Callback;
 import retrofit2.Response;
 
 /**
@@ -38,7 +38,6 @@ import retrofit2.Response;
 public class BrowserMoviesActivityFragment extends Fragment {
 
     private MovieAdapter m_MovieAdapter;
-    private IMovieService m_MovieService;
     private List<Movie> m_Movies;
     private ProgressDialog m_ProgressDialog;
     private String m_SortType = Constants.SORT_POPULAR_PARAM;
@@ -54,15 +53,11 @@ public class BrowserMoviesActivityFragment extends Fragment {
                              Bundle savedInstanceState) {
         View rootView = inflater.inflate(R.layout.fragment_browser_movies, container, false);
 
-        m_ProgressDialog = new ProgressDialog(getContext());
-        m_ProgressDialog.setTitle(R.string.loading);
-
         m_Movies = new ArrayList<>();
         m_MovieAdapter = new MovieAdapter(getActivity(), m_Movies);
 
         GridView gridViewMovie = (GridView) rootView.findViewById(R.id.gridViewMovie);
         gridViewMovie.setAdapter(m_MovieAdapter);
-        m_MovieService = MovieClient.createService(IMovieService.class);
 
         gridViewMovie.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
@@ -72,13 +67,15 @@ public class BrowserMoviesActivityFragment extends Fragment {
             }
         });
 
-        if(isOnline()) {
-           m_ProgressDialog.show();
-
-            fetchMovies(m_SortType);
-        }
-
         return rootView;
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        if(isOnline()) {
+            new FetchMoviesTask().execute(Constants.SORT_POPULAR_PARAM);
+        }
     }
 
 
@@ -108,7 +105,6 @@ public class BrowserMoviesActivityFragment extends Fragment {
         switch(item.getItemId()) {
             case R.id.action_sort_popular:
                 if(isOnline()) {
-                    m_ProgressDialog.show();
                     changeSort(Constants.SORT_POPULAR_PARAM);
                     if(!m_MenuItemSortPopular.isChecked()) {
                         m_MenuItemSortPopular.setChecked(true);
@@ -117,7 +113,6 @@ public class BrowserMoviesActivityFragment extends Fragment {
                 return true;
             case R.id.action_sort_rating:
                 if (isOnline()) {
-                    m_ProgressDialog.show();
                     changeSort(Constants.SORT_RATE_PARAM);
                     if(!m_MenuItemSortRating.isChecked()) {
                         m_MenuItemSortRating.setChecked(true);
@@ -131,7 +126,7 @@ public class BrowserMoviesActivityFragment extends Fragment {
 
     private void changeSort(String sort) {
         m_SortType = sort;
-        fetchMovies(m_SortType);
+        new FetchMoviesTask().execute(m_SortType);
     }
 
     public boolean isOnline() {
@@ -139,31 +134,45 @@ public class BrowserMoviesActivityFragment extends Fragment {
         NetworkInfo networkInfo = connectivityManager.getActiveNetworkInfo();
         if(networkInfo == null || !networkInfo.isConnected() || !networkInfo.isAvailable()) {
             Toast.makeText(getContext(), getString(R.string.message_offline), Toast.LENGTH_SHORT).show();
-            m_ProgressDialog.dismiss();
             return false;
         }
         return true;
     }
 
-    public void fetchMovies(String sort) {
-        Call<ResponseAPI<Movie>> moviesCall = m_MovieService.getMovies(sort);
+    public class FetchMoviesTask extends AsyncTask<String, Void, List<Movie>> {
 
-        moviesCall.enqueue(new Callback<ResponseAPI<Movie>>() {
-            @Override
-            public void onResponse(Call<ResponseAPI<Movie>> call, Response<ResponseAPI<Movie>> response) {
-                List<Movie> movieList = response.body().getResults();
-                m_MovieAdapter.clear();
-                for(Movie movie : movieList) {
+        IMovieService m_MovieService;
+        List<Movie> movieList = null;
+
+        @Override
+        protected void onPreExecute() {
+            m_ProgressDialog = ProgressDialog.show(getContext(), "Loading...", "Searching movies...");
+        }
+
+        @Override
+        protected List<Movie> doInBackground(String... params) {
+            String sort = params[0];
+            m_MovieService = MovieClient.createService(IMovieService.class);
+
+            Call<ResponseAPI<Movie>> moviesCall = m_MovieService.getMovies(sort);
+            try {
+                Response<ResponseAPI<Movie>> response = moviesCall.execute();
+                movieList = response.body().getResults();
+            } catch (Exception e) {
+                Toast.makeText(getContext(), getString(R.string.message_offline), Toast.LENGTH_SHORT).show();
+            }
+            return movieList;
+        }
+
+        @Override
+        protected void onPostExecute(List<Movie> result) {
+            m_MovieAdapter.clear();
+            if(result != null) {
+                for(Movie movie : result) {
                     m_MovieAdapter.add(movie);
                 }
-                m_ProgressDialog.dismiss();
             }
-
-            @Override
-            public void onFailure(Call<ResponseAPI<Movie>> call, Throwable t) {
-                Toast.makeText(getContext(), getString(R.string.message_offline), Toast.LENGTH_SHORT).show();
-                m_ProgressDialog.dismiss();
-            }
-        });
+            m_ProgressDialog.dismiss();
+        }
     }
 }
